17f1eeb0ecaa5c36abd90661ae849d860be912de
alexcharlton <alex.charlton@tensure.io>
2024-03-18 19:01:55 -0400

    prettier-config, babelrc-setup, handleClick-added

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,7 @@
 import React, { useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import styled from 'styled-components';
 import Header from './components/Header';
 import Joke from "./components/Joke";
-
-
-
-
-
-
-
-
-
-// const Background = styled.div`
-//   height: 100vh;
-//   width: 100visualViewport;
-//   background: rgb(111, 143, 175);
-//   display: flex;
-//   justify-content: center;
-//   align-items: center;
-// `
-
-// const StyledSquare = styled.div`
-// width: 100px; 
-// height: 100px;
-// border: 2px black solid;
-// `;
 
 const App = () => {
   return (
--- src/components/Joke.tsx
+++ src/components/Joke.tsx
@@ -1,12 +1,17 @@
 import React from "react"
 
 export default function Joke() {
+    function handleClick(){
+        console.log("click")
+    }
     return (
         <main>
-            <form>
-                <input type= "text" />
-                <input type= "text" />
-                <button>Press for Pop's Puns</button>
+            <form className ="form">
+                <button
+                    className="form--button"
+                    onClick={handleClick}
+                >
+                    Press for Pop's Puns</button>
             </form>
         </main>
     )

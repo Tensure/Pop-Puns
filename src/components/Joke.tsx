b411474ab55b6e5704063c721427e5c75642a4bf
alexcharlton <alex.charlton@tensure.io>
2024-03-25 20:17:57 -0400

    getJoke implimented with API and useState. Speech bubble css added.

--- src/components/Joke.tsx
+++ src/components/Joke.tsx
@@ -1,18 +1,31 @@
-import React from "react"
+import React, { useState } from "react"
 
 export default function Joke() {
-    function handleClick(){
-        console.log("click")
+    const [joke, setJoke] = useState(null);
+
+    function getJoke(){
+        fetch("https://icanhazdadjoke.com/", {
+            headers: {
+                Accept: "application/json"
+            }
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data.joke);
+            setJoke(data.joke);
+        })
     }
+
     return (
         <main>
-            <form className ="form">
+            <p className="bubble">{joke}</p>
+            <div className ="form">
                 <button
                     className="form--button"
-                    onClick={handleClick}
+                    onClick={getJoke}
                 >
                     Press for Pop's Puns</button>
-            </form>
+            </div>
         </main>
     )
 }
